70eea9579553278e8d62a910ec5262dd146edb42
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2025-02-02 17:16:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,4 +17,5 @@ SUDO_USERS = [int(_x) for _x in SUDO_USERS]
 
 AUTH_CHATS = environ.get("AUTH_CHATS").split()
 AUTH_CHATS = [int(_x) for _x in AUTH_CHATS]
-LOG_CHANNEL = environ.get("LOG_CHANNEL")
+#LOG_CHANNEL = environ.get("LOG_CHANNEL")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001997285269"))

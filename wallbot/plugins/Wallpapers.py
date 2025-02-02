c4a67fd97c65ea2556144964bc3f7805cc66f645
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2025-02-02 20:38:42 +0530

    Update Wallpapers.py

--- wallbot/plugins/Wallpapers.py
+++ wallbot/plugins/Wallpapers.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from pyrogram import filters
 from random import choice
-
+import requests
 from wallbot import wbot
 from wallbot.images_db.walls import ANIM_PICS, ANIMALS_PICS, LOGO_PICS, CARS_PICS, DROWIG_PICS, FUNNY_PICS, ENTERT_PICS, GAME_PICS, LOVE_PICS, MUSIC_PICS, NATURE_PICS, SAYING_PICS, SPACE_PICS, COMIC_PICS, SPORT_PICS, PATTER_PICS, TECHNO_PICS, DESIN_PICS, HOLDAY_PICS, PEOPL_PICS, OTHERS_PICS                      
 from config import LOG_CHANNEL, Telegram, DB_URL, DB_NAME

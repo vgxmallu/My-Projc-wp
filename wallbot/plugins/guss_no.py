e5492ed88b04d22fe6018280d576004ae212b164
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2025-06-05 23:43:10 +0530

    Update guss_no.py

--- wallbot/plugins/guss_no.py
+++ wallbot/plugins/guss_no.py
@@ -7,31 +7,31 @@ user_games = {}
 
 
 
-@app.on_message(filters.command("play"))
-def play(client, message):
+@wbot.on_message(filters.command("play"))
+async def play(client, message):
     num = random.randint(1, 100)
     user_games[message.from_user.id] = num
-    message.reply("I'm thinking of a number between 1 and 100. Try to guess it!")
+    await message.reply("I'm thinking of a number between 1 and 100. Try to guess it!")
 
-@app.on_message(filters.text & ~filters.command("play"))
-def guess(client, message):
+@wbot.on_message(filters.text & ~filters.command("play"))
+async def guess(client, message):
     user_id = message.from_user.id
     if user_id not in user_games:
-        message.reply("Type /play to start a game first!")
+        await message.reply("Type /play to start a game first!")
         return
 
     try:
         guess = int(message.text)
     except ValueError:
-        message.reply("Please send a valid number.")
+        await message.reply("Please send a valid number.")
         return
 
     number = user_games[user_id]
     if guess < number:
-        message.reply("Too low! Try again.")
+        await message.reply("Too low! Try again.")
     elif guess > number:
-        message.reply("Too high! Try again.")
+        await message.reply("Too high! Try again.")
     else:
-        message.reply("🎉 Correct! You guessed the number!")
+        await message.reply("🎉 Correct! You guessed the number!")
         del user_games[user_id]
 

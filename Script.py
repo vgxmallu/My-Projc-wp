ab27341956ef2d0a056d9edd1b887893fff0a883
vgxmallu <DeltaAlgor@outlook.com>
2025-08-16 23:36:37 +0530

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,397 @@
+
+
+ID_TEXT = """
+<b>Firstname</b>: <code>{}</code>
+<b>Lastname</b>: <code>{}</code>
+<b>Username</b>: <code>{}</code>
+<b>ID</b>: <code>{}</code>
+<b>Datacenter</b>: <code>{}</code>
+<b>ChatID</b>: <code>{}</code>
+"""
+
+STATUS_TXT = """<b>
+
+👤 Total Users: <code>{}</code>
+
+🗄️ Server: <code>Koyeb</code>
+
+💾 Memory: <code>2GB</code>
+
+🖥️ CPU: <code>8 Cores Xeon</code>
+
+🌐 Uptime: <code>69.99%</code>
+
+🧩 Bot Version: <code>v0.1.1</code>
+</b>
+
+"""
+
+block_chain = [
+             "🟥",
+             "🟧",
+             "🟨",
+             "🟩",
+             "🟦",
+             "🟪",
+             "🟫",
+             "⬛",
+             "⬜",
+             "🟥",
+             "🟧",
+             "🟨",
+             "🟩",
+             "🟦",
+             "🟪",
+             "🟫",
+             "⬛",
+             "⬜"
+]
+
+love_siren = [
+            "❤️❤️❤️🧡🧡🧡💚💚💚\n💙💙💙💜💜💜🖤🖤🖤",
+            "🖤🖤🖤💜💜💜💙💙💙\n❤️❤️❤️🧡🧡🧡💚💚💚",
+            "💛💛💛💙💙💙❤️❤️❤️\n💜💜💜❤️❤️❤️🧡🧡🧡",
+            "❤️❤️❤️🧡🧡🧡💚💚💚\n💙💙💙💜💜💜🖤🖤🖤",
+            "🖤🖤🖤💜💜💜💙💙💙\n❤️❤️❤️🧡🧡🧡💚💚💚",
+            "💛💛💛💙💙💙❤️❤️❤️\n💜💜💜❤️❤️❤️🧡🧡🧡",
+            "❤️❤️❤️🧡🧡🧡💚💚💚\n💙💙💙💜💜💜🖤🖤🖤",
+            "🖤🖤🖤💜💜💜💙💙💙\n❤️❤️❤️🧡🧡🧡💚💚💚",
+            "💛💛💛💙💙💙❤️❤️❤️\n💜💜💜❤️❤️❤️🧡🧡🧡"
+]
+
+
+hack_you = [
+            "Looking for Telegram databases in targeted person...",
+            " User online: True\nTelegram access: True\nRead Storage: True ",
+            "Hacking... 20.63%\n[███░░░░░░░░░░░░░░░░░]",    
+            "Hacking... 86.21%\n[███████████████░░░░░]",
+            "Hacking... 93.50%\n[█████████████████░░░]",
+            "hacking....  100%\n[████████████████████]",
+]
+
+police_siren = [
+            "🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵\n🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵\n🔴🔴🔴⬜️⬜️⬜️🔵🔵🔵",
+            "🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴\n🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴\n🔵🔵🔵⬜️⬜️⬜️🔴🔴🔴"
+]
+
+
+
+
+bomb_ettu = [
+             "▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️",
+             "💣💣💣💣\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️", 
+             "▪️▪️▪️▪️\n💣💣💣💣\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️",
+             "▪️▪️▪️▪️\n▪️▪️▪️▪️\n💣💣💣💣\n▪️▪️▪️▪️\n▪️▪️▪️▪️",
+             "▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n💣💣💣💣\n▪️▪️▪️▪️",
+             "▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n💣💣💣💣",
+             "▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n💥💥💥💥",
+             "▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n💥💥💥💥\n💥💥💥💥",
+             "▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n▪️▪️▪️▪️\n😵😵😵😵",
+]
+
+RUN_STRINGS = (
+    "Where do you think you're going?",
+    "Huh? what? did they get away?",
+    "ZZzzZZzz... Huh? what? oh, just them again, nevermind.",
+    "Get back here!",
+    "Not so fast...",
+    "Look out for the wall!",
+    "Don't leave me alone with them!!",
+    "You run, you die.",
+    "Jokes on you, I'm everywhere",
+    "You're gonna regret that...",
+    "You could also try /punchme, I hear that's fun.",
+    "Go bother someone else, no-one here cares.",
+    "You can run, but you can't hide.",
+    "Is that all you've got?",
+    "I'm behind you...",
+    "You've got company!",
+    "We can do this the easy way, or the hard way.",
+    "You just don't get it, do you?",
+    "Yeah, you better run!",
+    "Please, remind me how much I care?",
+    "I'd run faster if I were you.",
+    "That's definitely the droid we're looking for.",
+    "May the odds be ever in your favour.",
+    "Famous last words.",
+    "And they disappeared forever, never to be seen again.",
+    "\"Oh, look at me! I'm so cool, I can run from a bot!\" - this person",
+    "Yeah yeah, just tap /punchme already.",
+    "Here, take this ring and head to Mordor while you're at it.",
+    "Legend has it, they're still running...",
+    "Unlike Harry Potter, your parents can't protect you from me.",
+    "Fear leads to anger. Anger leads to hate. Hate leads to suffering. If you keep running in fear, you might "
+    "be the next Vader.",
+    "Multiple calculations later, I have decided my interest in your shenanigans is exactly 0.",
+    "Legend has it, they're still running.",
+    "Keep it up, not sure we want you here anyway.",
+    "You're a wiza- Oh. Wait. You're not Harry, keep moving.",
+    "NO RUNNING IN THE HALLWAYS!",
+    "Hasta la vista, baby.",
+    "Who let the dogs out?",
+    "It's funny, because no one cares.",
+    "Ah, what a waste. I liked that one.",
+    "Frankly, my dear, I don't give a damn.",
+    "My milkshake brings all the boys to yard... So run faster!",
+    "You can't HANDLE the truth!",
+    "A long time ago, in a galaxy far far away... Someone would've cared about that. Not anymore though.",
+    "Hey, look at them! They're running from the inevitable banhammer... Cute.",
+    "Han shot first. So will I.",
+    "What are you running after, a white rabbit?",
+    "As The Doctor would say... RUN!",
+)
+
+
+PING_STRING = (
+    "PONG!!",
+    "I am here!",
+)
+
+ITEMS = (
+    "cast iron skillet",
+    "angry meow",
+    "cricket bat",
+    "wooden cane",
+    "shovel",
+    "toaster",
+    "book",
+    "laptop",
+    "rubber chicken",
+    "spiked bat",
+    "heavy rock",
+    "chunk of dirt",
+    "ton of bricks",
+    "rasengan",
+    "spirit bomb",
+    "100-Type Guanyin Bodhisattva",
+    "rasenshuriken",
+    "Murasame",
+    "ban",
+    "chunchunmaru",
+    "Kubikiribōchō",
+    "rasengan",
+    "spherical flying kat",
+)
+
+THROW = (
+    "എറിഞ്ഞു",
+    "വിക്ഷേപിച്ചു",
+    "തട്ടി",
+    "വീശിയെറിഞ്ഞു",
+)
+
+HIT = (
+    "അടിച്ചു",
+    "ശക്തിയായി പ്രഹരിച്ചു",
+    "തല്ലി",
+    "ഇടിച്ചു",
+    "തൊഴിച്ചു",
+
+)
+
+ABUSE_STRINGS = (
+    "Fuck off",
+    "Stfu go fuck yourself",
+    "Ur mum gey",
+    "Ur dad lesbo",
+    "Bsdk",
+    "Nigga",
+    "Ur granny tranny",
+    "you noob",
+    "Relax your Rear,ders nothing to fear,The Rape train is finally here",
+    "Stfu bc",
+    "Stfu and Gtfo U nub",
+    "GTFO bsdk",
+    "CUnt",
+    " Gay is here",
+    "Ur dad gey bc ",
+)
+
+EYES = [
+    ['⌐■', '■'],
+    [' ͠°', ' °'],
+    ['⇀', '↼'],
+    ['´• ', ' •`'],
+    ['´', '`'],
+    ['`', '´'],
+    ['ó', 'ò'],
+    ['ò', 'ó'],
+    ['⸌', '⸍'],
+    ['>', '<'],
+    ['Ƹ̵̡', 'Ʒ'],
+    ['ᗒ', 'ᗕ'],
+    ['⟃', '⟄'],
+    ['⪧', '⪦'],
+    ['⪦', '⪧'],
+    ['⪩', '⪨'],
+    ['⪨', '⪩'],
+    ['⪰', '⪯'],
+    ['⫑', '⫒'],
+    ['⨴', '⨵'],
+    ['⩿', '⪀'],
+    ['⩾', '⩽'],
+    ['⩺', '⩹'],
+    ['⩹', '⩺'],
+    ['◥▶', '◀◤'],
+    ['◍', '◎'],
+    ['/͠-', '┐͡-\\'],
+    ['⌣', '⌣”'],
+    [' ͡⎚', ' ͡⎚'],
+    ['≋'],
+    ['૦ઁ'],
+    ['  ͯ'],
+    ['  ͌'],
+    ['ළ'],
+    ['◉'],
+    ['☉'],
+    ['・'],
+    ['▰'],
+    ['ᵔ'],
+    [' ﾟ'],
+    ['□'],
+    ['☼'],
+    ['*'],
+    ['`'],
+    ['⚆'],
+    ['⊜'],
+    ['>'],
+    ['❍'],
+    ['￣'],
+    ['─'],
+    ['✿'],
+    ['•'],
+    ['T'],
+    ['^'],
+    ['ⱺ'],
+    ['@'],
+    ['ȍ'],
+    ['  '],
+    ['  '],
+    ['x'],
+    ['-'],
+    ['$'],
+    ['Ȍ'],
+    ['ʘ'],
+    ['Ꝋ'],
+    [''],
+    ['⸟'],
+    ['๏'],
+    ['ⴲ'],
+    ['◕'],
+    ['◔'],
+    ['✧'],
+    ['■'],
+    ['♥'],
+    [' ͡°'],
+    ['¬'],
+    [' º '],
+    ['⨶'],
+    ['⨱'],
+    ['⏓'],
+    ['⏒'],
+    ['⍜'],
+    ['⍤'],
+    ['ᚖ'],
+    ['ᴗ'],
+    ['ಠ'],
+    ['σ'],
+    ['☯']
+]
+
+MOUTHS = [
+    ['v'],
+    ['ᴥ'],
+    ['ᗝ'],
+    ['Ѡ'],
+    ['ᗜ'],
+    ['Ꮂ'],
+    ['ᨓ'],
+    ['ᨎ'],
+    ['ヮ'],
+    ['╭͜ʖ╮'],
+    [' ͟ل͜'],
+    [' ͜ʖ'],
+    [' ͟ʖ'],
+    [' ʖ̯'],
+    ['ω'],
+    [' ³'],
+    [' ε '],
+    ['﹏'],
+    ['□'],
+    ['ل͜'],
+    ['‿'],
+    ['╭╮'],
+    ['‿‿'],
+    ['▾'],
+    ['‸'],
+    ['Д'],
+    ['∀'],
+    ['!'],
+    ['人'],
+    ['.'],
+    ['ロ'],
+    ['_'],
+    ['෴'],
+    ['ѽ'],
+    ['ഌ'],
+    ['⏠'],
+    ['⏏'],
+    ['⍊'],
+    ['⍘'],
+    ['ツ'],
+    ['益'],
+    ['╭∩╮'],
+    ['Ĺ̯'],
+    ['◡'],
+    [' ͜つ']
+]
+
+EARS = [
+    ['q', 'p'],
+    ['ʢ', 'ʡ'],
+    ['⸮', '?'],
+    ['ʕ', 'ʔ'],
+    ['ᖗ', 'ᖘ'],
+    ['ᕦ', 'ᕥ'],
+    ['ᕦ(', ')ᕥ'],
+    ['ᕙ(', ')ᕗ'],
+    ['ᘳ', 'ᘰ'],
+    ['ᕮ', 'ᕭ'],
+    ['ᕳ', 'ᕲ'],
+    ['(', ')'],
+    ['[', ']'],
+    ['¯\\_', '_/¯'],
+    ['୧', '୨'],
+    ['୨', '୧'],
+    ['⤜(', ')⤏'],
+    ['☞', '☞'],
+    ['ᑫ', 'ᑷ'],
+    ['ᑴ', 'ᑷ'],
+    ['ヽ(', ')ﾉ'],
+    ['\\(', ')/'],
+    ['乁(', ')ㄏ'],
+    ['└[', ']┘'],
+    ['(づ', ')づ'],
+    ['(ง', ')ง'],
+    ['⎝', '⎠'],
+    ['ლ(', 'ლ)'],
+    ['ᕕ(', ')ᕗ'],
+    ['(∩', ')⊃━☆ﾟ.*'],
+]
+
+TOSS = (
+    "Heads",
+    "Tails",
+)
+
+DECIDE = (
+    "Yes.",
+    "NoU.",
+    "Maybe."
+)
+
+TABLE = (
+    "(╯°□°）╯彡 ┻━┻",
+    "I ran out of tables, will order more.",
+    "Go do some work instead of flippin tables."
+)

b16920dfb5c8ac560a91251bcc1d58f5b73a5192
vgxmallu <DeltaAlgor@outlook.com>
2025-08-31 10:56:27 +0530

    Update nightmd.py

--- wallbot/plugins/nightmd.py
+++ wallbot/plugins/nightmd.py
@@ -7,11 +7,11 @@ from pyrogram.types import ChatPermissions
 from wallbot.database.nightdb import nightdb,nightmode_on,nightmode_off,get_nightchats 
 
 
-"""
+
 CLOSE_CHAT = ChatPermissions(
     can_send_messages=False,
     can_send_media_messages = False,
-    can_send_other_messages = False,
+    #can_send_other_messages = False,
     can_send_polls = False,
     can_change_info = False,
     can_add_web_page_previews = False,
@@ -22,13 +22,13 @@ CLOSE_CHAT = ChatPermissions(
 OPEN_CHAT = ChatPermissions(
     can_send_messages=True,
     can_send_media_messages = True,
-    can_send_other_messages = True,
+    #can_send_other_messages = True,
     can_send_polls = True,
     can_change_info = True,
     can_add_web_page_previews = True,
     can_pin_messages = True,
     can_invite_users = True )
-    """
+    
     
 buttons = InlineKeyboardMarkup([[InlineKeyboardButton("✓ On", callback_data="add_night"),InlineKeyboardButton("× Off", callback_data="rm_night")]])         
 
@@ -76,13 +76,13 @@ async def start_nightmode() :
                 photo="https://telegra.ph//file/06649d4d0bbf4285238ee.jpg",
                 caption= f"**ᴍᴀʏ ᴛʜᴇ ᴀɴɢᴇʟs ғʀᴏᴍ ʜᴇᴀᴠᴇɴ ʙʀɪɴɢ ᴛʜᴇ sᴡᴇᴇᴛᴇsᴛ ᴏғ ᴀʟʟ ᴅʀᴇᴀᴍs ғᴏʀ ʏᴏᴜ. ᴍᴀʏ ʏᴏᴜ ʜᴀᴠᴇ ʟᴏɴɢ ᴀɴᴅ ʙʟɪssғᴜʟ sʟᴇᴇᴘ ғᴜʟʟ ᴏғ ʜᴀᴘᴘʏ ᴅʀᴇᴀᴍs.\n\nɢʀᴏᴜᴘ ɪs ᴄʟᴏsɪɴɢ ɢᴏᴏᴅ ɴɪɢʜᴛ ᴇᴠᴇʀʏᴏɴᴇ  !**")
             
-            #await bot.set_chat_permissions(add_chat,CLOSE_CHAT)
+            await bot.set_chat_permissions(add_chat,CLOSE_CHAT)
 
         except Exception as e:
             print(f"[bold red] Unable To close Group {add_chat} - {e}")
 
 scheduler = AsyncIOScheduler(timezone="Asia/Kolkata")
-scheduler.add_job(start_nightmode, trigger="cron", hour=23, minute=59)
+scheduler.add_job(start_nightmode, trigger="cron", hour=10, minute=58) #23-59
 scheduler.start()
 
 async def close_nightmode():
@@ -99,7 +99,7 @@ async def close_nightmode():
                 photo="https://telegra.ph//file/14ec9c3ff42b59867040a.jpg",
                 caption= f"**ɢʀᴏᴜᴘ ɪs ᴏᴘᴇɴɪɴɢ ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ᴇᴠᴇʀʏᴏɴᴇ !\n\nᴍᴀʏ ᴛʜɪs ᴅᴀʏ ᴄᴏᴍᴇ ᴡɪᴛʜ ᴀʟʟ ᴛʜᴇ ʟᴏᴠᴇ ʏᴏᴜʀ ʜᴇᴀʀᴛ ᴄᴀɴ ʜᴏʟᴅ ᴀɴᴅ ʙʀɪɴɢ ʏᴏᴜ ᴇᴠᴇʀʏ sᴜᴄᴄᴇss ʏᴏᴜ ᴅᴇsɪʀᴇ. Mᴀʏ ᴇᴀᴄʜ ᴏғ ʏᴏᴜʀ ғᴏᴏᴛsᴛᴇᴘs ʙʀɪɴɢ Jᴏʏ ᴛᴏ ᴛʜᴇ ᴇᴀʀᴛʜ ᴀɴᴅ ʏᴏᴜʀsᴇʟғ. ɪ ᴡɪsʜ ʏᴏᴜ ᴀ ᴍᴀɢɪᴄᴀʟ ᴅᴀʏ ᴀɴᴅ ᴀ ᴡᴏɴᴅᴇʀғᴜʟ ʟɪғᴇ ᴀʜᴇᴀᴅ.**")
             
-            #await bot.set_chat_permissions(rm_chat,OPEN_CHAT)
+            await bot.set_chat_permissions(rm_chat,OPEN_CHAT)
 
         except Exception as e:
             print(f"[bold red] Unable To open Group {rm_chat} - {e}")

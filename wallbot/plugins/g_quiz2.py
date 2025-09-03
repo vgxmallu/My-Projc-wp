f729129dbff0895a7a07ff485833d4a6b5ade916
vgxmallu <DeltaAlgor@outlook.com>
2025-09-03 08:36:50 +0530

    Update g_quiz2.py

--- wallbot/plugins/g_quiz2.py
+++ wallbot/plugins/g_quiz2.py
@@ -30,7 +30,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, 
 from pymongo import MongoClient, ASCENDING, DESCENDING
 from config import DB_URL
 from wallbot import wbot as app
-from questions import QUESTIONS
+from questions2 import QUESTIONS
 # ---------------- Config ----------------
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("adv_quiz_bot")

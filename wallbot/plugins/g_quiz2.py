4f630c8edbc738f267f80547788ba3112f1fe4e0
vgxmallu <DeltaAlgor@outlook.com>
2025-09-04 23:21:12 +0530

    Update g_quiz2.py

--- wallbot/plugins/g_quiz2.py
+++ wallbot/plugins/g_quiz2.py
@@ -118,7 +118,7 @@ def record_group_score(chat_id: int, user_id: int, username: str, points: int):
 
 # ---------- Commands ----------
 @app.on_message(filters.command("start_qz") & filters.private)
-async def cmd_cg4challenge(_, message: Message):
+async def cmd_cqz4challenge(_, message: Message):
     g = await message.reply("Use this Command on private chats not here❌")
     await asyncio.sleep(60)
     await message.delete()

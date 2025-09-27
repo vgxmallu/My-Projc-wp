92e3c6916590bc533b4cf176c0d8b61800f4b686
vgxmallu <DeltaAlgor@outlook.com>
2025-09-27 21:37:21 +0530

    Update m_anti_flood.py

--- wallbot/plugins/m_anti_flood.py
+++ wallbot/plugins/m_anti_flood.py
@@ -214,18 +214,13 @@ async def flood_check(client: Client, message: Message):
             # Mute permissions object (no send rights)
             mute_perms = ChatPermissions(
                 can_send_messages=False,
-                #can_send_media_messages=False,
+                can_send_audios=False,
                 can_send_documents=False,
                 can_send_photos=False,
                 can_send_videos=False,
                 can_send_video_notes=False,
                 can_send_voice_notes=False,
                 can_send_polls=False,
-                can_add_web_page_previews=False,
-                can_change_info=False,
-                can_invite_users=False,
-                can_pin_messages=False
-                can_send_audios=False,
                 can_send_other_messages=False,
                 can_add_web_page_previews=False
             )

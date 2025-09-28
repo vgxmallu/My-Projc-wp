78d2f36a3f764dc458a523c323dc85fbcf2d0783
vgxmallu <DeltaAlgor@outlook.com>
2025-09-28 13:00:28 +0530

    Update m_anti_flood.py

--- wallbot/plugins/m_anti_flood.py
+++ wallbot/plugins/m_anti_flood.py
@@ -216,7 +216,6 @@ async def flood_check(client: Client, message: Message):
                 can_send_messages=False,
                 can_send_media_messages=False,
                 can_send_polls=False,
-                can_send_other_messages=False,
                 can_add_web_page_previews=False,
                 can_change_info=False,
                 can_invite_users=False,

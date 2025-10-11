ea53d2ee90efd7ed1bcf1c36438dd02e6148cc05
vgxmallu <DeltaAlgor@outlook.com>
2025-10-11 08:18:28 +0530

    Update __main__.py

--- wallbot/__main__.py
+++ wallbot/__main__.py
@@ -4,14 +4,9 @@ import sys
 import asyncio
 from wallbot import wbot 
 
-async def main():
-    bot = wbot()
-    await bot.start()
-    await idle()  # wait until Ctrl+C or SIGTERM
-    await bot.stop()
 
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        LOGGER.info("Bot shutting down...")
+
+if name == "main":
+    if not path.exists("cache"):
+        mkdir("cache")
+    wbot().run()

d7da3a7e5d25f20101581ffaec874201531ba196
vgxmallu <DeltaAlgor@outlook.com>
2025-10-19 18:32:59 +0530

    Update quots_shedul.py

--- wallbot/plugins/quots_shedul.py
+++ wallbot/plugins/quots_shedul.py
@@ -16,6 +16,9 @@ from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from enums import ParseMode
+
+
 
 load_dotenv()
 
@@ -69,13 +72,13 @@ async def startgg_cmd(_, m: Message):
         "/settime HH:MM - Set delivery time (UTC)\n"
         "/quote - Get a random quote now",
     )
-
+#parse_mode=ParseMode.MARKDOWN.value
 @bot.on_message(filters.command("quote"))
 async def quote_cmd(_, m: Message):
     q = await fetch_quote()
     if not q:
         return await m.reply_text("❌ Couldn't fetch a quote.")
-    await m.reply_text(build_quote(q), parse_mode="html")
+    await m.reply_text(build_quote(q), parse_mode=ParseMode.HTML.value)
 
 @bot.on_message(filters.command("qtsubscribe"))
 async def subscribxxe_cmd(client, m: Message):
@@ -90,7 +93,7 @@ async def subscribxxe_cmd(client, m: Message):
         {"$set": {"chat_id": m.chat.id, "enabled": True, "send_time": time, "chat_type": m.chat.type}},
         upsert=True
     )
-    await m.reply_text(f"✅ Subscribed! Daily quote at <b>{time} UTC</b>.", parse_mode="html")
+    await m.reply_text(f"✅ Subscribed! Daily quote at <b>{time} UTC</b>.", parse_mode=ParseMode.HTML.value)
 
 @bot.on_message(filters.command("qtunsubscribe"))
 async def unsubxxscribe_cmd(client, m: Message):
@@ -108,6 +111,6 @@ async def sgsettime_cmd(client, m: Message):
     if not t:
         return await m.reply_text("❌ Invalid format. Use HH:MM (UTC)")
     await client.subs.update_one({"chat_id": m.chat.id}, {"$set": {"send_time": t}}, upsert=True)
-    await m.reply_text(f"🕒 Time updated to <b>{t} UTC</b>.", parse_mode="html")
+    await m.reply_text(f"🕒 Time updated to <b>{t} UTC</b>.", parse_mode=ParseMode.HTML.value)
 
 # ---------------- RUN ----------------

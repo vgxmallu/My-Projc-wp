0285fb3eafcbc1f4a02681c18d3c628d1b69c478
vgxmallu <DeltaAlgor@outlook.com>
2025-10-19 18:42:26 +0530

    Update quots_shedul.py

--- wallbot/plugins/quots_shedul.py
+++ wallbot/plugins/quots_shedul.py
@@ -17,6 +17,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from pyrogram import Client, filters, enums 
 from pyrogram.types import Message
 #from enums import ParseMode
+from wallbot import wbot as bot
 
 
 

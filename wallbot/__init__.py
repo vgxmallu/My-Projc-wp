4b346370ce5f046e6e51cd4c890e5785aa495e12
vgxmallu <DeltaAlgor@outlook.com>
2025-10-24 21:57:44 +0530

    Update __init__.py

--- wallbot/__init__.py
+++ wallbot/__init__.py
@@ -32,7 +32,7 @@ class wbot(Client):
         name = self.__class__.__name__.lower()
         super().__init__(
             ":memory:",
-            plugins=dict(root=f"{name}/plugins"),
+            plugins=dict(root="wallbot/plugins"),
             workdir="./cache/",
             api_id=API_ID,
             api_hash=API_HASH,

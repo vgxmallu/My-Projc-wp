ff3937f2f6c15e51af73e7cd0320f6d87143eeb2
vgxmallu <DeltaAlgor@outlook.com>
2025-10-24 21:40:00 +0530

    Update __init__.py

--- wallbot/__init__.py
+++ wallbot/__init__.py
@@ -1,83 +1,60 @@
-import asyncio
-import time
-from asyncio import get_event_loop
-from logging import ERROR, INFO, StreamHandler, basicConfig, getLogger, handlers
-from aiohttp import ClientSession
+
+
+import os
 import logging
+from os import environ, mkdir, path, sys
+from dotenv import load_dotenv
 from pyrogram import Client
+from aiohttp import ClientSession
+import time
+from dotenv import load_dotenv
 
 from config import API_ID, API_HASH, BOT_TOKEN, DB_URL
-from wallbot.plugins.word import load_words, load_common_words
-
-
-from apscheduler.jobstores.mongodb import MongoDBJobStore
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-
-from motor.motor_asyncio import AsyncIOMotorClient
-#from async_pymongo import AsyncClient
-from pymongo import MongoClient
-#from motor import motor_asyncio
 
 
 
-# enable logging
+
+
+# Log
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
-    level=logging.INFO,
+    level=logging.DEBUG,
+    format="%(asctime)s - %(message)s",
+    handlers=[logging.FileHandler("bot.log"), logging.StreamHandler()],
 )
-
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
 LOGGER = logging.getLogger(__name__)
 
-getLogger("pyrogram").setLevel(ERROR)
 
 
 
-#mongo = AsyncIOMotorClient(DB_URL)
-#db = mongo["Gomezgames"]
+class wbot(Client):
+    def __init__(self):
+        name = self.__class__.__name__.lower()
+        super().__init__(
+            ":memory:",
+            plugins=dict(root=f"{name}/plugins"),
+            workdir="./cache/",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=BOT_TOKEN,
+            sleep_threshold=30,
+        )
 
-#Telethon bot
-#tle = TelegramClient("telethn", API_ID, API_HASH, flood_sleep_threshold=0).start(bot_token=BOT_TOKEN)
-#print("⚫⚪TELETHON IS STARTED...⚫⚪")
+    async def start(self):
+        global BOT_INFO
+        await super().start()
+        BOT_INFO = await self.get_me()
+        if not path.exists("/tmp/thumbnails/"):
+            mkdir("/tmp/thumbnails/")
+        for chat in AUTH_CHATS:
+            await self.send_photo(
+                chat,
+                "https://files.catbox.moe/80bcxh.jpg",
+                "**GimezGames Bot as Started.**🎮✅",
+            )
+        LOGGER.info(f"GomezGamesbot STARTED AS {BOT_INFO.username}\n")
 
-# Pyrogram Bot Client
-wbot = Client(
-    "Gomezgames",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    mongodb=dict(connection=AsyncIOMotorClient(DB_URL), remove_peers=True),
-    sleep_threshold=180,
-    app_version="MissKatyPyro Stable",
-    workers=50,
-    max_concurrent_transmissions=4,
-)
-wbot.db = AsyncIOMotorClient(DB_URL)["wallbot_db"]
-LOGGER.info(f"✅ Bot started")
+    async def stop(self, *args):
+        await super().stop()
+        LOGGER.info("Gomezgames STOPPED BRO.")
 
-
-# ---------------- Database + Word Setup ----------------
-DEV_LIST = [784589736]
-
-client = AsyncIOMotorClient(DB_URL)
-db = client["WordNWord"]
-user_Collection = db["user"]
-collection = db["word"]
-
-WORD_LIST = set(load_words())
-WORD_SET = set(WORD_LIST)
-MEAN_WORD = load_common_words()
-MEAN_WORD_SET = set(MEAN_WORD)
-
-print(f"Loaded {len(WORD_SET)} words from the word list.")
-# ---------------- Database + Word Setup ----------------
-
-
-#jobstores = {
-#    "default": MongoDBJobStore(
-#        client=MongoClient(DATABASE_URI), database=DATABASE_NAME, collection="nightmode"
-#    )
-#}
-#scheduler = AsyncIOScheduler(jobstores=jobstores, timezone=TZ)
-
-wbot.start()

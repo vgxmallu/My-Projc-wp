89fd1f94e67df88802a9af0e4af78765789328cc
vgxmallu <DeltaAlgor@outlook.com>
2025-11-02 18:49:43 +0530

    Update __main__.py

--- wallbot/__main__.py
+++ wallbot/__main__.py
@@ -6,6 +6,16 @@ import asyncio
 import logging
 from pyrogram import idle
 
+
+from pyrogram import Client
+from wallbot.plugins.g_daily_brain import run_background_tasks
+
+@wbot.on_start()
+async def on_start(client):
+    client.loop.create_task(run_background_tasks())
+    print("✅ Background task started automatically when bot runs.")
+    
+
 if __name__ == "__main__":
     if not path.exists("cache"):
         mkdir("cache")

dba1838e98f75d8db9e7ea249e497c734d31edac
vgxmallu <DeltaAlgor@outlook.com>
2025-11-09 22:25:02 +0530

    Update all_repo.py

--- wallbot/plugins/all_repo.py
+++ wallbot/plugins/all_repo.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 import requests
-from wallbot import wbot as app
+from main import wbot as app
 
 # Function to chunk the repository info into smaller parts
 def chunk_string(text, chunk_size):

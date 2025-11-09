9fbbb90ab61e5af4f56760be491206c2b04709ba
vgxmallu <DeltaAlgor@outlook.com>
2025-11-09 22:37:44 +0530

    Update g_emoji_equation.py

--- wallbot/plugins/g_emoji_equation.py
+++ wallbot/plugins/g_emoji_equation.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import DB_URL
-from wallbot import wbot as app
+from main import wbot as app
 
 
 

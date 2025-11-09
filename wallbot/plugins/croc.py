d0e803f21aa9985d0d460a35c8df6d7429831c2a
vgxmallu <DeltaAlgor@outlook.com>
2025-11-09 22:29:08 +0530

    Update croc.py

--- wallbot/plugins/croc.py
+++ wallbot/plugins/croc.py
@@ -1,9 +1,9 @@
 import random
 from pyrogram import filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from wallbot import wbot as word
+from main import wbot as word
 
-from wallbot import WORD_SET, MEAN_WORD_SET
+from main import WORD_SET, MEAN_WORD_SET
 from wallbot.database.db import update_stats, get_stats
 
 

12f55b958f76d49088207a76eb5fee70cd498f15
vgxmallu <DeltaAlgor@outlook.com>
2025-11-10 20:09:19 +0530

    Update whisper.py

--- wallbot/plugins/whisper.py
+++ wallbot/plugins/whisper.py
@@ -4,7 +4,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pymongo import MongoClient
 from datetime import datetime, timedelta
 from config import DB_URL
-from wallbot import wbot as app
+from main import wbot as app
 DB_NAME = "whisper_db"
 EXPIRE_MINUTES = 30  # whispers auto-delete after 30 minutes
 # ----------------------------------------
